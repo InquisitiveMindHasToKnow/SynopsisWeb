d26a056c41ae3d3e992360850f121a149ad48574
InquisitiveMindHasToKnow <omarraymond18@gmail.com>
2024-07-09 16:13:33 -0400

    Removed unnecessary Lets connect button in Banner.js. Removed excess from footer. Modified footer css in App.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,7 @@
-import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/images/logo.svg"
-import firstNavIcon from '../assets/images/nav-icon1.svg'
-import secondNavIcon from '../assets/images/nav-icon2.svg'
-import thirdNavIcon from '../assets/images/nav-icon3.svg'
-
 export const Footer = () => {
     return (
-        <footer className="Footer">
-            <Container>
-                <Row className="align-item-center">
-                    <Col sm={6}>
-                        <img src={logo} alt="Logo" />
-                    </Col>
-                    <Col sm={6} className="text-center text-sm-end">
-                        <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/omarraymond"><img src={firstNavIcon} alt="Omar LinkedIn" /> </a>
-                            <a href="https://github.com/InquisitiveMindHasToKnow"><img src={secondNavIcon} alt="Omar Github"/></a>
-                            <a href="#"><img src={thirdNavIcon} alt="Icon"/></a>
-                        </div>
-                        <p>CopyRight 2024, All Rights Reserved</p>
-                    </Col>
-                </Row>
-            </Container>
+        <footer className="footer">
+            <p>CopyRight 2024, All Rights Reserved</p>
         </footer>
-    )
-}
+    );
+};
