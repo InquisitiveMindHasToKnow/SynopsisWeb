ff5b70b5392dec5b62d2fa8550a9f6ad616f11c0
InquisitiveMindHasToKnow <omarraymond18@gmail.com>
2024-07-09 02:24:45 -0400

    Changed second icon from facebook to github. added alt to image in ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,7 @@ export const ProjectCard = ({ title, description, imageUrl}) => {
     return (
         <Col sm={6} md={4}>
           <div className="proj-imgbx">
-                <img src={imageUrl} />
+                <img src={imageUrl} alt=""/>
                 <div className="proj-txtx">
                     <h4> {title} </h4>
                     <span> {description} </span>
