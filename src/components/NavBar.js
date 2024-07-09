fc25f9f34be477c8eff7a2f19e091f7b202fc5fa
InquisitiveMindHasToKnow <omarraymond18@gmail.com>
2024-07-09 15:36:24 -0400

    Refactors NavBar.js. Renamed Contacts to Contact and added it to App.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,12 @@ import firstNavIcon from '../assets/images/nav-icon1.svg'
 import secondNavIcon from '../assets/images/nav-icon2.svg'
 import thirdNavIcon from '../assets/images/nav-icon3.svg'
 
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+
+
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home')
     const[scrolled, setScrolled] = useState(false)
@@ -32,32 +38,34 @@ export const NavBar = () => {
     }
 
   return (
-    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
-      <Container>
-        <Navbar.Brand href="#home">
-            <img src = {logo} alt = "Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle> aria-controls="basic-navbar-nav" 
-        <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("home")}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("skills")}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("projects")}>Projects</Nav.Link>
-          </Nav>
-          <span className='navbar-text'>
-                <div className='social-icon'> 
-                    <a href="https://www.linkedin.com/in/omarraymond"><img src={firstNavIcon} alt="Omar LinkedIn" /> </a>
-                    <a href="https://github.com/InquisitiveMindHasToKnow"><img src={secondNavIcon} alt="Omar Github"/></a>
-                    <a href="#"><img src={thirdNavIcon} alt="" /> </a>
-                </div>
-                <button className='scroll-to-skills' onClick={() => console.log("go to contact us form")}>
-                    <span >Let's Connect</span>
-                </button>
-          </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+            <div className='social-icon'> 
+                     <a href="https://www.linkedin.com/in/omarraymond"><img src={firstNavIcon} alt="Omar LinkedIn" /> </a>
+                     <a href="https://github.com/InquisitiveMindHasToKnow"><img src={secondNavIcon} alt="Omar Github"/></a>
+                     <a href="#"><img src={thirdNavIcon} alt="" /> </a>
+                 </div>
+              <HashLink to='#connect'>
+                <button className="vvd"><span>Let’s Connect</span></button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  )
 }
